96dc4f493dcc61bdd4957fac13375aa6a2085b65
Lucas Bollella <lucasbollella@MacBook-Pro-de-Lucas.local>
2022-05-06 20:09:13 -0300

    Agrego una receta real para ver el estilo

--- src/components/data/recetas/recetas.js
+++ src/components/data/recetas/recetas.js
@@ -2,26 +2,43 @@ export const recetas = [
     {
         id:1,
         categoria: {
-            descripcion: "Sopas y verduras",
+            descripcion: "Pollos",
             id:1
         },
-        nombre: "Pollo frito",
-        descripcion:"Yesterday, as Facebook launched its news reader app Paper, design-focused startup FiftyThree called out Facebook...",
+        nombre: "Pollo al verdeo con crema liviana",
+        descripcion:"Técnicamente, la crema liviana que acompaña este pollo al verdeo, reduce la intensidad de la siesta que te mandás después del plato. Está científicamente comprobado y así lo enseñan en las escuelas de alta gastronomía.",
         ingredientes: [
-            "100g de jamon",
-            "Un chorrito de aceite"
+            "1 pechuga de pollo grande",
+            "5 cebollas de verdeo",
+            "1 diente de ajo",
+            "1 chorro de vino blanco",
+            "1 calidto",
+            "100cc de crema de leche",
+            "Sal, pimienta y aceite"
         ],
         preparacion:[
             {
                 paso:1,
-                descripcion:"Lava el pollo"
+                descripcion:"Cortar la pechuga en trozos grandes. Calentar el aceite y dorar los trozos de ambos lados. No deben quedar demasiado dorados, apenas."
             },
             {
                 paso:2,
-                descripcion:"Metetelo en el culo"
+                descripcion:"Agregar al pollo las cebollas de verdeo, cortadas en rodajas gruesas, y el ajo picado. Cocer a fuego fuerte. Apenas un par de minutos, la cebolla de verdeo no debe ablandarse tanto como en otros sofritos."
+            },
+            {
+                paso:3,
+                descripcion:"Agregar el chorro de vino y dejar al fuego uno o dos minutos, para que evapore el alcohol"
+            },
+            {
+                paso:4,
+                descripcion:"Agregar el caldito, la sal, la pimienta y agua hasta cubrir apenas el pollo. Poner a fuego fuerte hasta que hierva, luego bajar el fuego y cocinar, tapado, una media hora."
+            },
+            {
+                paso:5,
+                descripcion:"Una vez que esté listo, dejar unos minutos para que baje el calor y agregar la crema de leche. Mezclar hasta que la salsa se una. Quedará una salsa cremosa pero liviana. Servir acompañado de arroz como yo, puré, papas o lo que se te ocurra."
             },
         ],
-        imagen:"/assets/recetas/receta1.jpg",
+        imagen:"/assets/recetas/pollo-con-crema.jpeg",
         usuario:{
             id:1,
             nombre:"Pedro Sanchez"
--- src/components/recetas/detalle/DetalleReceta.jsx
+++ src/components/recetas/detalle/DetalleReceta.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Image, ListGroup, Accordion, Container, Row } from 'react-bootstrap'
+import { Image, ListGroup, Accordion, Container, Row, Col } from 'react-bootstrap'
 import { useParams, Navigate } from 'react-router-dom'
 import { getRecetaById } from '../../../domain/service/recetas/RecetasService'
 import './DetalleRecetaStyle.scss'
@@ -17,24 +17,36 @@ export const DetalleReceta = () => {
  <Container>
    <Row className="justify-content-md-center">
 {/*   <div className='container mt-5 center  float-start float-sm-start' >
- */}        <h1 className='title'>{receta.nombre}</h1>
-        <h2 className='descrip'>{receta.descripcion}</h2>
-        <hr className='mb-0'/>
-        <div className='float-start float-sm-start mt-3' style={{ width: '370px' }}>
-          <Image src={receta.imagen} style={{ width: '350px' }}/>
-        </div>
-        <div className='float-start border-start p-3' style={{ width: '350px' }}>
-          <div>
-            <h3 className='mt-3'>Ingredientes</h3>
-            <ListGroup as="ol" numbered>
-              {
-                receta.ingredientes.map(ingrediente => {
-                  return <ListGroup.Item as="li">{ingrediente}</ListGroup.Item>
-                })
-              }
-              
-            </ListGroup>
-          </div >
+ */}        
+    <h1 className='titleReceta'>{receta.nombre}</h1>
+    <h2 className='descrip'>{receta.descripcion}</h2>
+    <hr className='mb-0'/>
+
+        <Row>
+          <div className='alineo'>
+            <Image src={receta.imagen} style={{ width: '75%', padding:'20px', borderRadius:'24px'}} />
+          </div> 
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col xs={12}  md ={9} lg={9}>
+                <h3 className='titulo'>Ingredientes</h3>
+
+                <ListGroup variant='flush'>
+                  {
+                    receta.ingredientes.map(ingrediente => {
+                      return <ListGroup.Item as="li">{ingrediente}</ListGroup.Item>
+                    })
+                  }
+                  
+                </ListGroup>
+
+          </Col>
+
+        </Row>
+
+        
+        <div className='float-start border-start p-3' style={{ width: '600px' }}>
+
           <div className='mt-3 mb-5 float-start' style={{ width: '350px' }}>
             <h3>Preparacion</h3>
             <Accordion>
